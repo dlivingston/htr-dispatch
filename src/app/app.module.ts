a9010d497aafcaa1dc6d02333d06b59a18317ac0
Don Livingston <donlivingston@gmail.com>
2017-10-02 19:45:15 -0600

    live firebase config data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,12 +23,12 @@ import { NewServiceNoteComponent } from './new-service-note/new-service-note.com
 //import { O2UploadToFbsComponent } from 'o2-upload-to-fbs';
 import { UploadFormComponent } from './uploads/upload-form/upload-form.component';
 export const firebaseConfig = {
-	apiKey: "AIzaSyCu1LlPQz0CBXj4qdnShbcU3xHPGTdn22c",
-    authDomain: "htr-beta.firebaseapp.com",
-    databaseURL: "https://htr-beta.firebaseio.com",
-    projectId: "htr-beta",
-    storageBucket: "htr-beta.appspot.com",
-    messagingSenderId: "519074297252"
+	apiKey: "AIzaSyAH2iBrD3Pu0FO9lolVnKEmVviYAdBOptc",
+    authDomain: "htr-dispatch.firebaseapp.com",
+    databaseURL: "https://htr-dispatch.firebaseio.com",
+    projectId: "htr-dispatch",
+    storageBucket: "htr-dispatch.appspot.com",
+    messagingSenderId: "151102819118"
 };
 
 const appRoutes: Routes = [
